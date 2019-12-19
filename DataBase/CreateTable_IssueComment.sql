adcb5855c8935219457208f84b89cc12f07dabf8
t105590017 <t105590017@ntut.org.tw>
2019-12-19 19:54:01 +0800

    [似乎打成全型了 但是我修好了哦

--- DataBase/CreateTable_IssueComment.sql
+++ DataBase/CreateTable_IssueComment.sql
@@ -4,7 +4,7 @@ USE ITS;
 CREATE TABLE [IssueComment] (
     [IssueId] INT,                    -- 問題流水號
     [CommentId] INT,                  -- 留言流水號
-    [CommenterId] INT,                -- 留言人
+    [CommenterId] INT,                -- 留言人
     [Content] NVARCHAR(255 ),         -- 留言內容
     [CreateTime] DATETIME,            -- 留言問題時間
     [CreateUesr] INT,                 -- 留言人 Id
