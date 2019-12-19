20d157a38aa68283e679994319aa1102d66ca960
chris <f98989000@gmail.com>
2019-12-20 00:02:22 +0800

    add service layer

---
+++ IssueTrackingSystemApi/IssueTrackingSystem.Services/IUserService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IssueTrackingSystemApi.Models;
+
+namespace IssueTrackingSystemApi.Services
+{
+    public interface IUserService
+    {
+        /// <summary>
+        /// 依照Id取得User
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        User GetUserById(int id);
+
+        /// <summary>
+        /// 更新User
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        int UpdateUsers(int id);
+
+        /// <summary>
+        /// 創建User
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        int CreateUser(User user);
+    }
+}
---
+++ IssueTrackingSystemApi/IssueTrackingSystem.Services/IssueService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IssueTrackingSystemApi.Models;
+
+namespace IssueTrackingSystemApi.Services
+{
+    public class IssueService : IIssueService
+    {
+        public IssueService()
+        {
+        }
+
+        public int CreateIssue(Issue issue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Issue GetIssue(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Issue> Issues()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateIssue(Issue issue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
