9b92736c5c4b76a9e7a0eb7a8648ddc7e1b0eb15
t105590017 <t105590017@ntut.org.tw>
2019-12-23 17:47:41 +0800

    NMS 要開API 先MERGE

---
+++ IssueTrackingSystemApi/IssueTrackingSystemApi/Controllers/NotificationMessageSubsystemController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IssueTrackingSystemApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace IssueTrackingSystemApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationMessageSubsystemController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public NotificationMessageSubsystemController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        [HttpPost]
+        [Route("CheckUserLoginInfo")]
+        public IActionResult CheckUserLoginInfo([FromBody] User user)
+        {
+            if(string.IsNullOrEmpty(user.Account) ||
+               string.IsNullOrEmpty(user.Password)||
+               string.IsNullOrEmpty(user.LineId)) // 檢核所需資料
+            {
+                return BadRequest();
+            }
+
+
+
+            return BadRequest();
+        }
+
+    }
+}
