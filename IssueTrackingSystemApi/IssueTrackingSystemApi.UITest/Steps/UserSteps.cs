62c8e7df1d0e0d87108f73e54f95904fc7bd8f13
chris_fs_wang <f98989000@gmail.com>
2019-12-25 02:02:34 +0800

    add specFlow User feature

---
+++ IssueTrackingSystemApi/IssueTrackingSystemApi.UITest/Steps/UserSteps.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+using SpecFlowStudy.UiTests.CommonTools;
+using System;
+using TechTalk.SpecFlow;
+
+namespace IssueTrackingSystemApi.UITest.Steps
+{
+    [Binding]
+    public class UserSteps
+    {
+        private WebDriver _webDriver;
+
+        [BeforeScenario]
+        public void GetWebDriver()
+        {
+            _webDriver = new WebDriver();
+        }
+
+        [Given(@"我前往網頁 (.*)")]
+        public void Given我前往網頁(string url)
+        {
+            IWebDriver webDriver = _webDriver.Current;
+            webDriver.Manage().Window.Maximize();
+            webDriver.Navigate().GoToUrl(string.Format("{0}{1}", webDriver.Url, url));
+        }
+        
+        [Then(@"首頁應顯示 (.*)")]
+        public void Then首頁應顯示(string title)
+        {
+            var result = _webDriver.Wait.Until(ExpectedConditions.ElementIsVisible(By.Id("account")));
+            Assert.AreEqual(title, _webDriver.Current.Title);
+        }
+
+        [Then(@"我輸入帳號 (.*)")]
+        public void Then我輸入帳號Admin(string account)
+        {
+            var element = _webDriver.Wait.Until(ExpectedConditions.ElementIsVisible(By.Id("account")));
+            element.SendKeys(account);
+        }
+
+        [Then(@"我輸入密碼 (.*)")]
+        public void Then我輸入密碼Admin(string password)
+        {
+            var element = _webDriver.Wait.Until(ExpectedConditions.ElementIsVisible(By.Id("password")));
+            element.SendKeys(password);
+        }
+
+        [Then(@"首頁應顯示名稱 (.*)")]
+        public void Then首頁應顯示名稱(string name)
+        {
+            Assert.Fail();
+        }
+
+
+        [AfterScenario]
+        public void CloseBrowser()
+        {
+            _webDriver.Quit();
+        }
+    }
+}
---
+++ IssueTrackingSystemApi/IssueTrackingSystemApi.UITest/User.feature
@@ -0,0 +1,17 @@
+﻿Feature: User
+	In order to 使用ITS
+	As a 系統使用者
+	I want to 登入並使用ITS系統
+
+@basePage
+Scenario: 基本頁面
+	Given 我前往網頁 /
+	Then 首頁應顯示 issue-tracking-system-frontend
+
+@login
+Scenario: 登入
+	Given 我前往網頁 /
+	Then 我輸入帳號 Admin
+	And 我輸入密碼 admin
+	Then 首頁應顯示名稱 Kelvin
+
