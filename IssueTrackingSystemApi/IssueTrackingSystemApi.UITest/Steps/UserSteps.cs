9fb1824ffaf1c24bed22761bac623e39ce9e1b0d
chris_fs_wang <f98989000@gmail.com>
2019-12-25 12:45:17 +0800

    add scenarios

--- IssueTrackingSystemApi/IssueTrackingSystemApi.UITest/Steps/UserSteps.cs
+++ IssueTrackingSystemApi/IssueTrackingSystemApi.UITest/Steps/UserSteps.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium;
 using SeleniumExtras.WaitHelpers;
 using SpecFlowStudy.UiTests.CommonTools;
 using System;
+using System.Threading;
 using TechTalk.SpecFlow;
 
 namespace IssueTrackingSystemApi.UITest.Steps
@@ -34,23 +35,86 @@ namespace IssueTrackingSystemApi.UITest.Steps
         }
 
         [Then(@"我輸入帳號 (.*)")]
-        public void Then我輸入帳號Admin(string account)
+        public void Then我輸入帳號(string account)
         {
             var element = _webDriver.Wait.Until(ExpectedConditions.ElementIsVisible(By.Id("account")));
             element.SendKeys(account);
         }
 
         [Then(@"我輸入密碼 (.*)")]
-        public void Then我輸入密碼Admin(string password)
+        public void Then我輸入密碼(string password)
         {
             var element = _webDriver.Wait.Until(ExpectedConditions.ElementIsVisible(By.Id("password")));
             element.SendKeys(password);
         }
 
+        [Then(@"我按下登入按鈕")]
+        public void Then我按下登入按鈕()
+        {
+            var element = _webDriver.Wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//button[@type='submit']")));
+            element.Click();
+        }
+
+        [Then(@"我按下個人資料的頭像")]
+        public void Then我按下個人資料的頭像()
+        {
+            var element = _webDriver.Wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector(".fa-user > path")));
+            element.Click();
+        }
+
+        [Then(@"我按下Profile按鈕")]
+        public void Then我按下Profile按鈕()
+        {
+            var element = _webDriver.Wait.Until(ExpectedConditions.ElementIsVisible(By.LinkText("Profile")));
+            element.Click();
+        }
+
+        [Then(@"個人資料應該為")]
+        public void Then個人資料應該為(Table table)
+        {
+            var name = _webDriver.Wait.Until(ExpectedConditions.ElementIsVisible(By.Id("input-name")));
+            var account = _webDriver.Wait.Until(ExpectedConditions.ElementIsVisible(By.Id("input-account")));
+            var email = _webDriver.Wait.Until(ExpectedConditions.ElementIsVisible(By.Id("input-eMail")));
+            var authority = _webDriver.Wait.Until(ExpectedConditions.ElementIsVisible(By.Id("input-charactorId")));
+            Assert.AreEqual(table.Rows[0]["名稱"], name.GetAttribute("value"));
+            Assert.AreEqual(table.Rows[0]["帳號"], account.GetAttribute("value"));
+            Assert.AreEqual(table.Rows[0]["信箱"], email.GetAttribute("value"));
+            Assert.AreEqual(table.Rows[0]["權限"], authority.GetAttribute("value"));
+        }
+
+        [Then(@"我修改個人資料並按下修改按鈕")]
+        public void Then我修改個人資料並按下修改按鈕(Table table)
+        {
+            var name = _webDriver.Wait.Until(ExpectedConditions.ElementIsVisible(By.Id("input-name")));
+            var account = _webDriver.Wait.Until(ExpectedConditions.ElementIsVisible(By.Id("input-account")));
+            var email = _webDriver.Wait.Until(ExpectedConditions.ElementIsVisible(By.Id("input-eMail")));
+            name.Clear();
+            name.SendKeys(table.Rows[0]["名稱"]);
+            account.Clear();
+            account.SendKeys(table.Rows[0]["帳號"]);
+            email.Clear();
+            email.SendKeys(table.Rows[0]["信箱"]);
+            var button = _webDriver.Wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("(//button[@type='button'])[3]")));
+            button.Click();
+        }
+
+        [Then(@"應出現 (.*) 提示訊息")]
+        public void Then應出現修改成功提示訊息(string message)
+        {
+            Thread.Sleep(1000);
+            var alert = _webDriver.Current.SwitchTo().Alert();
+            string textOnAlert = alert.Text;
+            Assert.AreEqual(message, alert.Text);
+            alert.Accept();
+        }
+
+
+
         [Then(@"首頁應顯示名稱 (.*)")]
         public void Then首頁應顯示名稱(string name)
         {
-            Assert.Fail();
+            var element = _webDriver.Wait.Until(ExpectedConditions.ElementIsVisible(By.LinkText("Kelvin")));
+            Assert.AreEqual(name, element.Text);
         }
 
 
--- IssueTrackingSystemApi/IssueTrackingSystemApi.UITest/User.feature.cs
+++ IssueTrackingSystemApi/IssueTrackingSystemApi.UITest/User.feature.cs
@@ -75,14 +75,11 @@ namespace IssueTrackingSystemApi.UITest
         
         [NUnit.Framework.TestAttribute()]
         [NUnit.Framework.DescriptionAttribute("基本頁面")]
-        [NUnit.Framework.CategoryAttribute("basePage")]
         public virtual void 基本頁面()
         {
-            string[] tagsOfScenario = new string[] {
-                    "basePage"};
-            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("基本頁面", null, new string[] {
-                        "basePage"});
-#line 7
+            string[] tagsOfScenario = ((string[])(null));
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("基本頁面", null, ((string[])(null)));
+#line 6
 this.ScenarioInitialize(scenarioInfo);
 #line hidden
             bool isScenarioIgnored = default(bool);
@@ -102,10 +99,10 @@ this.ScenarioInitialize(scenarioInfo);
             else
             {
                 this.ScenarioStart();
-#line 8
+#line 7
  testRunner.Given("我前往網頁 /", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line hidden
-#line 9
+#line 8
  testRunner.Then("首頁應顯示 issue-tracking-system-frontend", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
             }
@@ -113,15 +110,12 @@ this.ScenarioInitialize(scenarioInfo);
         }
         
         [NUnit.Framework.TestAttribute()]
-        [NUnit.Framework.DescriptionAttribute("登入")]
-        [NUnit.Framework.CategoryAttribute("login")]
-        public virtual void 登入()
+        [NUnit.Framework.DescriptionAttribute("登入系統")]
+        public virtual void 登入系統()
         {
-            string[] tagsOfScenario = new string[] {
-                    "login"};
-            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("登入", null, new string[] {
-                        "login"});
-#line 12
+            string[] tagsOfScenario = ((string[])(null));
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("登入系統", null, ((string[])(null)));
+#line 10
 this.ScenarioInitialize(scenarioInfo);
 #line hidden
             bool isScenarioIgnored = default(bool);
@@ -141,17 +135,143 @@ this.ScenarioInitialize(scenarioInfo);
             else
             {
                 this.ScenarioStart();
-#line 13
+#line 11
  testRunner.Given("我前往網頁 /", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line hidden
-#line 14
+#line 12
  testRunner.Then("我輸入帳號 Admin", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
-#line 15
+#line 13
  testRunner.And("我輸入密碼 admin", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
-#line 16
+#line 14
+ testRunner.And("我按下登入按鈕", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 15
  testRunner.Then("首頁應顯示名稱 Kelvin", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+            }
+            this.ScenarioCleanup();
+        }
+        
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.DescriptionAttribute("查看個人資料")]
+        public virtual void 查看個人資料()
+        {
+            string[] tagsOfScenario = ((string[])(null));
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("查看個人資料", null, ((string[])(null)));
+#line 17
+this.ScenarioInitialize(scenarioInfo);
+#line hidden
+            bool isScenarioIgnored = default(bool);
+            bool isFeatureIgnored = default(bool);
+            if ((tagsOfScenario != null))
+            {
+                isScenarioIgnored = tagsOfScenario.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((this._featureTags != null))
+            {
+                isFeatureIgnored = this._featureTags.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((isScenarioIgnored || isFeatureIgnored))
+            {
+                testRunner.SkipScenario();
+            }
+            else
+            {
+                this.ScenarioStart();
+#line 18
+ testRunner.Given("我前往網頁 /", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line hidden
+#line 19
+ testRunner.Then("我輸入帳號 ApiUITest", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+#line 20
+ testRunner.And("我輸入密碼 1234", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 21
+ testRunner.And("我按下登入按鈕", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 22
+ testRunner.And("我按下個人資料的頭像", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 23
+ testRunner.And("我按下Profile按鈕", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+                TechTalk.SpecFlow.Table table1 = new TechTalk.SpecFlow.Table(new string[] {
+                            "名稱",
+                            "帳號",
+                            "信箱",
+                            "權限"});
+                table1.AddRow(new string[] {
+                            "chrisTestAccount",
+                            "ApiUITest",
+                            "f98989000@gmail.com",
+                            "系統管理員"});
+#line 24
+ testRunner.Then("個人資料應該為", ((string)(null)), table1, "Then ");
+#line hidden
+            }
+            this.ScenarioCleanup();
+        }
+        
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.DescriptionAttribute("修改個人資料")]
+        public virtual void 修改個人資料()
+        {
+            string[] tagsOfScenario = ((string[])(null));
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("修改個人資料", null, ((string[])(null)));
+#line 28
+this.ScenarioInitialize(scenarioInfo);
+#line hidden
+            bool isScenarioIgnored = default(bool);
+            bool isFeatureIgnored = default(bool);
+            if ((tagsOfScenario != null))
+            {
+                isScenarioIgnored = tagsOfScenario.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((this._featureTags != null))
+            {
+                isFeatureIgnored = this._featureTags.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((isScenarioIgnored || isFeatureIgnored))
+            {
+                testRunner.SkipScenario();
+            }
+            else
+            {
+                this.ScenarioStart();
+#line 29
+ testRunner.Given("我前往網頁 /", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line hidden
+#line 30
+ testRunner.Then("我輸入帳號 ApiUITest", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+#line 31
+ testRunner.And("我輸入密碼 1234", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 32
+ testRunner.And("我按下登入按鈕", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 33
+ testRunner.And("我按下個人資料的頭像", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 34
+ testRunner.And("我按下Profile按鈕", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+                TechTalk.SpecFlow.Table table2 = new TechTalk.SpecFlow.Table(new string[] {
+                            "名稱",
+                            "帳號",
+                            "信箱"});
+                table2.AddRow(new string[] {
+                            "chrisTestAccountModified",
+                            "Admin",
+                            "t105590045@ntut.org.tw"});
+#line 35
+ testRunner.And("我修改個人資料並按下修改按鈕", ((string)(null)), table2, "And ");
+#line hidden
+#line 38
+ testRunner.Then("應出現 修改成功 提示訊息", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
             }
             this.ScenarioCleanup();
